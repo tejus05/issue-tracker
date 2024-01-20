9ec89733dd6b93e0e51f66363debf008b684d34a
tejus <thequantumcoder@gmail.com>
2024-01-20 21:23:30 +0530

    handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from '@/prisma/client'
 
 const createIssueSchema = z.object({
-  title:z.string().min(1).max(255),
-  description:z.string().min(1)
+  title:z.string().min(1, "Title is required. ").max(255),
+  description:z.string().min(1,"Description is required. ")
 })
 
 export async function POST(request: NextRequest) {
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
 
   const validation = createIssueSchema.safeParse(body);
 
-  if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
+  if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
     data: {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
-import { useForm, Controller } from 'react-hook-form';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { AiFillInfoCircle } from 'react-icons/ai';
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
 ssr: false,
 });
@@ -17,24 +19,42 @@ interface IssueForm{
 
 const NewIssuePage = () => {
   const router = useRouter();
+  const [error, setError] = useState('');
   const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async(data) => {
-      await axios.post('/api/issues', data);
-      router.push('/issues');
-    })}>
-      <TextField.Root>
-        <TextField.Input placeholder='Title' {...register('title')} />
-      </TextField.Root>
-      <Controller
-        name='description'
-        control={control}
-        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
-      />
-      <Button>
-        Submit New Issue
-      </Button>
-    </form>
+    <div className='max-w-xl space-y-3'>
+      { error && 
+      <div>
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Icon>
+            <AiFillInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>
+            {error}
+          </Callout.Text>
+        </Callout.Root>
+      </div>}
+      <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post('/api/issues', data);
+          router.push('/issues');
+        } catch (error) {
+          setError("An unexpected error occured. ");
+        }
+      })}>
+        <TextField.Root>
+          <TextField.Input placeholder='Title' {...register('title')} />
+        </TextField.Root>
+        <Controller
+          name='description'
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+        />
+        <Button className='hover:cursor-pointer'>
+          Submit New Issue
+        </Button>
+      </form>
+    </div>
   )
 }
 
