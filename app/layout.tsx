f19d61fdb73992d8a6285493d94a210850364f07
tejus <thequantumcoder@gmail.com>
2024-01-20 14:05:27 +0530

    added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
