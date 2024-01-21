bfc8952161f0a0d052f848b2930277422a3c37e0
tejus <thequantumcoder@gmail.com>
2024-01-21 19:03:16 +0530

    styled the issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,9 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
+import validator from 'validator'
 
 interface Props {
   params: {
@@ -9,7 +12,11 @@ interface Props {
 }
 
 const IssueDetailPage = async ({params:{id}}:Props) => {
-  if (typeof id !== 'number') notFound();
+
+  if (!validator.isNumeric(id)) {
+    notFound();
+  }
+
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(id)
@@ -17,12 +24,15 @@ const IssueDetailPage = async ({params:{id}}:Props) => {
   })
 
   if (!issue) notFound();
+
   return (
     <div>
-      <p>{ issue.title }</p>
-      <p>{ issue.description }</p>
-      <p>{ issue.status }</p>
-      <p>{ issue.createdAt.toDateString() }</p>
+      <Heading>{ issue.title }</Heading>
+      <Flex gap="3" my='2'>
+        <IssueStatusBadge status={issue.status}/>
+        <Text>{ issue.createdAt.toDateString() }</Text>
+      </Flex>
+      <Card>{ issue.description }</Card>
     </div>
   )
 }
