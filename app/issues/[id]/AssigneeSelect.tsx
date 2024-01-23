1b37eb7acf584524607a4bd7fdfc155780822c9c
tejus <thequantumcoder@gmail.com>
2024-01-23 16:35:19 +0530

    refactor : assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,37 +1,34 @@
 "use client";
 
+import { Skeleton } from '@/app/components';
 import { Issue, User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { Skeleton } from '@/app/components';
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({issue}:{issue:Issue}) => {
-  const { data:users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000,
-    retry: 3
-  })
+  const { data: users, error, isLoading } = useUsers();
 
   if (error) return null;
 
   if(isLoading) return <Skeleton/>
 
+  const assignIssue = (userId:string) => {
+    axios.patch(`/api/issues/${issue.id}`, {
+      assignedToUserId: (
+        userId === "unassigned" ? null : userId
+      )
+    }).catch(() => {
+      toast.error("Changes could not be saved. ")
+    })
+  }
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || "unassigned"}
-        onValueChange={(userId) => {
-          axios.patch(`/api/issues/${issue.id}`, {
-            assignedToUserId: (
-              userId === "unassigned" ? null : userId
-            )
-          }).catch(() => {
-            toast.error("Changes could not be saved. ")
-          })
-        }}>
+        onValueChange={assignIssue}>
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
           <Select.Group>
@@ -51,5 +48,12 @@ const AssigneeSelect = ({issue}:{issue:Issue}) => {
     </>
   )
 }
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: () => axios.get('/api/users').then(res => res.data),
+  staleTime: 60 * 1000,
+  retry: 3
+})
 
 export default AssigneeSelect
