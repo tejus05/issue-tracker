5584d157ecbc3125ed3ab4dea0851d7b6f091c15
tejus <thequantumcoder@gmail.com>
2024-01-23 15:14:59 +0530

    enhance the api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { createIssueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -6,61 +6,87 @@ import authOptions from "../../auth/authOptions";
 
 interface Props {
   params: {
-    id: string
-  }
+    id: string;
+  };
 }
 
-export async function PATCH(request:NextRequest, {params:{id}}:Props) {
-
+export async function PATCH(request: NextRequest, { params: { id } }: Props) {
   const session = await getServerSession(authOptions);
 
-  if (!session) return NextResponse.json({ error: "unauthorised" }, { status: 401 });
+  if (!session)
+    return NextResponse.json({ error: "unauthorised" }, { status: 401 });
 
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
-  if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { title, description, assignedToUserId } = validation.data;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: assignedToUserId,
+      },
+    });
+
+    if (!user)
+      return NextResponse.json({ error: "invalid user" }, { status: 404 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
-      id: parseInt(id)
-    }
-  })
+      id: parseInt(id),
+    },
+  });
 
-  if (!issue) return NextResponse.json({ error: "issue does not exist. " }, { status: 404 })
-  
+  if (!issue)
+    return NextResponse.json(
+      { error: "issue does not exist. " },
+      { status: 404 }
+    );
+
   const updatedIssue = await prisma.issue.update({
     where: {
-      id: issue.id
+      id: issue.id,
     },
     data: {
-      title: validation.data?.title,
-      description: validation.data?.description
-    }
-  })
+      title,
+      description,
+      assignedToUserId
+    },
+  });
 
-  return NextResponse.json(updatedIssue,{status: 200});
+  return NextResponse.json(updatedIssue, { status: 200 });
 }
 
-export async function DELETE(request: NextRequest, {params:{id}}:Props) {
-
+export async function DELETE(request: NextRequest, { params: { id } }: Props) {
   const session = await getServerSession(authOptions);
 
-  if (!session) return NextResponse.json({ error: "unauthorised" }, { status: 401 });
+  if (!session)
+    return NextResponse.json({ error: "unauthorised" }, { status: 401 });
 
   const issue = await prisma.issue.findUnique({
     where: {
-      id: parseInt(id)
-    }
-  })
+      id: parseInt(id),
+    },
+  });
 
-  if (!issue) return NextResponse.json({ error: "issue does not exist" }, { status: 404 });
+  if (!issue)
+    return NextResponse.json(
+      { error: "issue does not exist" },
+      { status: 404 }
+    );
 
   await prisma.issue.delete({
     where: {
-      id: issue.id
-    }
-  })
+      id: issue.id,
+    },
+  });
 
-  return NextResponse.json({ message: "issue deleted successfully" }, { status: 200 });
+  return NextResponse.json(
+    { message: "issue deleted successfully" },
+    { status: 200 }
+  );
 }
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,31 @@
 import { z } from "zod";
 
 export const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required. ").max(100, "Title is too long."),
-  description: z.string().min(1, "Description is required.").max(255, "Description is too long."),
+  title: z
+    .string()
+    .min(1, "Title is required. ")
+    .max(100, "Title is too long."),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535, "Description is too long."),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required. ")
+    .max(100, "Title is too long.")
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535, "Description is too long.")
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(255)
+    .optional()
+    .nullable(),
 });
